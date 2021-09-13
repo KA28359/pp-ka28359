24a39eea16ff76816262253201626f3005bb85f7
Kevin Aguilar <47673410+KA28359@users.noreply.github.com>
2021-09-13 09:50:05 -0500

    tokenizer

--- java/code/Table.java
+++ java/code/Table.java
@@ -1,6 +1,69 @@
+import java.io.*;
+import java.util.ArrayList;
 
 public class Table {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        ArrayList<ArrayList<String>> tokens = new ArrayList<>();
+
+        File file = new File("1.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        tokens = tokenizer(br);
+
+        
+
+        /*
+        //DEBUGGING STATEMENT
+
+        for(ArrayList<String> a : tokens){
+            for(String s : a){
+                System.out.println(s);
+            }
+        }
+        */
+
+
+
     }
+
+    static boolean IsWhiteSpace(char c){
+        return c == ' ' || c == '\t' || c == '\r' || c == '\n';
+    }
+
+    static ArrayList<ArrayList<String>> tokenizer(BufferedReader br) throws IOException {
+
+        ArrayList<ArrayList<String>> tokens = new ArrayList<>();
+
+        String st;
+        String currentString = "";
+        int counter = 0;
+        while ((st = br.readLine()) != null) {
+            tokens.add(new ArrayList<>());
+            for (int i = 0; i < st.length(); i++) {
+                if (!IsWhiteSpace(st.charAt(i))) {
+
+                    String newVal = Character.toString(st.charAt(i));
+                    currentString = currentString + newVal;
+
+
+                }else{
+                    tokens.get(counter).add(currentString);
+                    //System.out.println(currentString);
+                    currentString = "";
+                    //clear string
+
+                }
+            }
+            tokens.get(counter).add(currentString);
+            //System.out.println(currentString);
+            currentString = "";
+            counter++;
+        }
+
+        return tokens;
+
+    }
+
 }
